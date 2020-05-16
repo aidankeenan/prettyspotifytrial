73d6bdff63bbd2841461972e5717350b4d3d0f41
Aidan Keenan <aidankeenan@Aidans-MacBook-Pro.local>
2020-05-16 13:27:19 -0400

    new get route

--- app.js
+++ app.js
@@ -57,9 +57,14 @@ app.get('/login', function(req, res) {
     }));
 });
 
-app.get('*', function(req, res){
+app.get('/', function(req, res){
   res.redirect('/callback');
 });
+
+app.get('/favicon.ico', function(req, res){
+  res.redirect('/callback');
+});
+
 
 app.get('/callback', function(req, res) {
 
@@ -121,30 +126,6 @@ app.get('/callback', function(req, res) {
       }
     });
   }
-});
-
-app.get('/refresh_token', function(req, res) {
-
-  // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
-    }
-  });
 });
 
 console.log('Listening on 8888');
